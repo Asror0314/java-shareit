ab8f52b36756ce655341059292190625af34f3f2
acer0314 <hudoykulov.asror95@gmail.com>
2023-02-08 11:57:52 +0500

    refactored configuration

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,29 +1,46 @@
 package ru.practicum.shareit.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 /**
  * TODO Sprint add-controllers.
  */
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "users")
+@Getter @Setter @ToString
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank
+    @Column(name = "username", nullable = false)
     private String name;
 
     @NotBlank
     @Email
-    @NotNull
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        return id != null && id.equals(((User) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
