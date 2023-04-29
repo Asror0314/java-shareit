cc39775fe89396f96c2729de15cf1bbf6f1ab622
Asror <hudoykulov.asror95@gmail.com>
2023-04-29 23:07:41 +0500

    fixed dtos

--- gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -5,13 +5,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentDto {
 
+    private long id;
+
     @NotBlank
     private String text;
+
+    private String authorName;
+
+    private LocalDateTime created;
 
 }
--- gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -3,14 +3,23 @@ package ru.practicum.shareit.request.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import ru.practicum.shareit.item.dto.ItemDtoForRequest;
 
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ItemRequestDto {
 
+    private long id;
+
     @NotBlank
     private String description;
+
+    private LocalDateTime created;
+
+    private List<ItemDtoForRequest> items;
 }
