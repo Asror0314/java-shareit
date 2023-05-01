1987e58ebc174a3facab4a9ad4fa6e0e30680b58
Asror <hudoykulov.asror95@gmail.com>
2023-05-01 17:53:47 +0500

    removed unnecessary imports

--- gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -5,8 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @NoArgsConstructor
